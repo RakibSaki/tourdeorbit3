4d7314657d8cde217b3c3d0f9ab67790a37f7107
Rakib Muhammed Saki <rakib1saki@gmail.com>
2022-04-01 17:04:19 +0600

    Create zoom, speed up and play/pause buttons

--- script.js
+++ script.js
@@ -8,8 +8,11 @@ let unitsZoom = 1 / 1e9     // 1 pixel shows 1 billion meters
 let userZoom = 1        // user can further control zoom
 let biggestVisible = () => (width + height) / (unitsZoom * userZoom)
 
+let itscale = 6311.52
 let tscale = 6311.52    // 1 real millisecond represents this many virtual seconds (so an earth year is shown in five seconds)
 let lastTime = 0
+
+let play = false
 
 function draw() {
     background(0)
@@ -20,13 +23,27 @@ function draw() {
     planet.drawVelocity()
     planet.draw()
     star.draw()
-    planet.move((millis() - lastTime) * tscale)
-    lastTime = millis()
+    if (play) {
+        planet.move((millis() - lastTime) * tscale)
+        lastTime = millis()
+    }
 }
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight)
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#play').addEventListener('click', e => {
+        if (!play) {
+            lastTime = millis()
+            e.target.innerHTML = 'pause_circle'
+        } else {
+            e.target.innerHTML = 'play_circle'
+        }
+        play = !play
+    })
+})
 
 function zoomIn() {
     userZoom *= 1.1
@@ -36,4 +53,14 @@ function zoomIn() {
 function zoomOut() {
     userZoom /= 1.1
     document.querySelector('#sscale').innerHTML = (150 / (userZoom * unitsZoom)).toPrecision(3)
+}
+
+function faster() {
+    tscale *= 1.1
+    document.querySelector('#tscale').innerHTML = (5 * itscale / tscale).toPrecision(3)
+}
+
+function slower() {
+    tscale /= 1.1
+    document.querySelector('#tscale').innerHTML = (5 * itscale / tscale).toPrecision(3)
 }
